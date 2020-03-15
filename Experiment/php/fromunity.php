8b9ccf24a48b99b0cb20acc23af26d2fd647cdb6
Véronne <veronners@live.nl>
2020-03-15 14:32:13 +0100

    try2

--- Experiment/php/fromunity.php
+++ Experiment/php/fromunity.php
@@ -5,12 +5,20 @@
 	
 	if($doc_name != "")
 	{
-		$file = fopen("https://github.com/veronners/DigitSpanTask/tree/master/Files/".$doc_name.".csv", "a");
+		$file = fopen($doc_name.".csv", "a");
 		fwrite($file, $song);
 		fwrite($file, $results);
 		fclose($file);
+		
+		header('Content-Type: application/octet-stream');
+		header('Content-Disposition: attachment; filename='.basename($doc_name.".csv"));
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate');
+		header('Pragma: public');
+		header('Content-Length: ' . filesize($doc_name.".csv"));
+		readfile($doc_name.".csv");
+		exit;
 	} else {
 		echo("Message delivery failed...");
-	}
-	
+	}	
 ?>
